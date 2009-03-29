56394f550f297edc705b819932b923e66c26aa08
Andrew Moreland <andy@andymo.org>
2009-03-28 21:23:08 -0700

    Made the lame test a little cleaner.. still not very good though.

--- tests/go_to_file_on_line_test.rb
+++ tests/go_to_file_on_line_test.rb
@@ -1,4 +1,15 @@
 # This isn't really a formal test.. it's just something I wrote in order to prove to myself that the regular expressions were working while I was working on them.  Just run this file with ruby go_to_file_on_line_test.rb.
+def test(regex, input, message)
+  puts "!!!!!!#{message}!!!!!!!"
+  input.each do |i|
+    if  i.match(regex)
+      puts "Works: #{i}" 
+      puts "$1: #{$1}"
+      puts "-------"
+    end
+  end
+end
+
 input = <<-END
 render :partial => @post
 render :partial => foo
@@ -12,21 +23,7 @@ END
 
 array = input.scan(/(.*)\n/).flatten
 regex = /render[\s\(].*:partial\s*=>\s*['"@]([\w\d\/]+)['",]{0,2}/
-
-def test(regex, input, message)
-  puts "!!!!!!#{message}!!!!!!!"
-  input.each do |i|
-    if  i.match(regex)
-      puts "Works: #{i}" 
-      puts "$1: #{$1}"
-      puts "-------"
-    end
-  end
-end
-
 test(regex, array, "testing first regex")
-
-regex = /render\s*\(?@([^'"), ]+)/
 
 input = <<-END
 render @post
@@ -37,5 +34,5 @@ foo
 END
 
 array = input.scan(/(.*)\n/).flatten
-
+regex = /render\s*\(?@([^'"), ]+)/
 test(regex, array, "testing second regex")
